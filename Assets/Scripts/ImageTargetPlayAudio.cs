63252390a15c34597035a5c8f9bd236e12d79149
Nhan Cao <nhancv92@gmail.com>
2017-10-13 15:04:46 +0700

    init

---
+++ Assets/Scripts/ImageTargetPlayAudio.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Vuforia
+{
+	public class ImageTargetPlayAudio : MonoBehaviour,
+										ITrackableEventHandler
+	{
+		private AudioSource audio;
+
+		private TrackableBehaviour trackableBehaviour;
+
+		void Start ()
+		{
+			audio = GetComponent<AudioSource> ();
+
+			trackableBehaviour = GetComponent<TrackableBehaviour> ();
+			if (trackableBehaviour) {
+				trackableBehaviour.RegisterTrackableEventHandler (this);
+			}
+
+		}
+
+		public void OnTrackableStateChanged (
+			TrackableBehaviour.Status previousStatus,
+			TrackableBehaviour.Status newStatus)
+		{
+			if (newStatus == TrackableBehaviour.Status.DETECTED ||
+			   newStatus == TrackableBehaviour.Status.TRACKED ||
+			   newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED) {
+
+				audio.Play ();
+			} else {
+
+				audio.Pause ();
+			}
+		}
+	}
+
+}
